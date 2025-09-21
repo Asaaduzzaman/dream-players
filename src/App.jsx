50ba7447e0eaa6b2f6abe7b6a3100ed5110a6b68
Asaaduzzaman <m4aboard@gmail.com>
2025-09-22 00:35:36 +0600

    added navbar img and other section

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import Coinsimg from "./assets/dollar-1.png"
 
   return (
     <>
-    <div className="navbar">
+    <div className="navbar max-w-[1200px] mx-auto">
   <div className="flex-1">
     <a className="text-xl"><img className="w-[60px] h-[60px]" src={Logoimg} alt="" /></a>
   </div>
   <div className="flex">
-     <span>6000000000</span>
-     <span>Coins</span>
+     <span className="mr-1">6000000000</span>
+     <span className="mr-1">Coins</span>
      <img src={Coinsimg} alt="" />
     
   </div>
